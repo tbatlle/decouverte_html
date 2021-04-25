31b56f0f606ea635055945b3998a6c37f92202b8
tbatlle <thierry.education81@gmail.com>
2021-04-26 00:22:10 +0200

    Création de la page d'index

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require 'include/header.php'; ?>
+<div class="container">
+    <h2>Menu</h2>    
+    <ul>
+        <li>
+            <a href="">Définition du HTML</a>
+        </li>
+        <li>
+            <a href="">Utilisation d'un environnement de développement</a>
+        </li>
+        <li>
+            <a href="">Création d'une page web</a>
+        </li>
+    </ul>
+    <h2 class='bg-light'>Définition du HTML</h2>
+    <h2 class='bg-light'>Utilisation d'un environnement de développement</h2>
+    <h2 class='bg-light'>Création d'une page web</h2>
+    
+</div>
+<?php require 'include/footer.php'; ?>
